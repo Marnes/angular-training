6e32c31288b690571ab9093638c1efac605ecb05
Marnes Prinsloo <marnes.prinsloo@gmail.com>
2025-01-21 10:42:54 +0200

    Exercise 3 implementation

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -9,16 +9,5 @@ import { HousingLocation } from '../housinglocation';
   styleUrl: './housing-location.component.scss'
 })
 export class HousingLocationComponent {
-
-  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
-  housingLocation: HousingLocation = {
-    id: 9999,
-    name: 'Test Home',
-    city: 'Test city',
-    state: 'ST',
-    photo: `${this.baseUrl}/example-house.jpg`,
-    availableUnits: 99,
-    wifi: true,
-    laundry: false,
-  };
+  @Input() housingLocation!: HousingLocation;
 }
